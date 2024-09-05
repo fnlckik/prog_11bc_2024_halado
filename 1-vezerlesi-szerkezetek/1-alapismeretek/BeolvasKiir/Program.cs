115f8ba00629bc7ead5a9f9563e60e33eb1dcdc7
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-05 12:19:06 +0200

    Heló Világ!

---
+++ 1-vezerlesi-szerkezetek/1-alapismeretek/BeolvasKiir/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace BeolvasKiir
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Helló Világ!");
+
+            Console.ReadKey();
+        }
+    }
+}
