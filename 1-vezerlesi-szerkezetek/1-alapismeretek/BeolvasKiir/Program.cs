cf4cf041bc58b182dc223e1f4f011aa5743b7f33
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-06 16:31:14 +0200

    WriteLine, ReadLine, típusok, konverzió

--- 1-vezerlesi-szerkezetek/1-alapismeretek/BeolvasKiir/Program.cs
+++ 1-vezerlesi-szerkezetek/1-alapismeretek/BeolvasKiir/Program.cs
@@ -6,7 +6,28 @@ namespace BeolvasKiir
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Helló Világ!");
+            // Console.WriteLine("Helló Világ!");
+
+            // 1. Olvasd be a felhasználó vezeték és
+            // keresztnevét és köszöntsd!
+            /*
+            string vnev, knev;
+
+            Console.Write("Vezetéknév: ");
+            vnev = Console.ReadLine();
+
+            Console.Write("Keresztnév: ");
+            knev = Console.ReadLine();
+
+            Console.WriteLine("Szia " + vnev + " " + knev + "!");
+            Console.WriteLine("Szia {0} {1}!", vnev, knev);
+            Console.WriteLine($"Szia {vnev} {knev}!");
+            */
+
+            // 2. Téglalap terület két oldalból
+            int a = Convert.ToInt32(Console.ReadLine());
+            int b = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine($"Terület: {a*b}");
 
             Console.ReadKey();
         }
