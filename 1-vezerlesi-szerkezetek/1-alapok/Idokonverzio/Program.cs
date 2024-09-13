2cfecc5c67d97c8d7df01c71a851cf8a34b38626
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-13 14:06:46 +0200

    Idokonverzio, Viz

---
+++ 1-vezerlesi-szerkezetek/1-alapok/Idokonverzio/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Idokonverzio
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int ora, perc, masodperc;
+            Random r = new Random();
+            ora = r.Next(24);
+            perc = r.Next(60);
+            masodperc = r.Next(60);
+
+            Console.WriteLine($"Időpont: {ora:00}:{perc:00}:{masodperc:00}");
+
+            int idopont = ora * 3600 + perc * 60 + masodperc;
+            Console.WriteLine($"A nap kezdete óta {idopont} másodperc telt el.");
+
+            // Váltsuk vissza!
+            int hour, minute, second;
+            hour = idopont / 3600;
+            int maradek = idopont % 3600;
+            minute = maradek / 60;
+            second = maradek % 60;
+
+            Console.WriteLine($"A nap kezdete óta {hour} óra {minute} perc és {second} másodperc telt el.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1-vezerlesi-szerkezetek/2-elagazasok/Viz/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Viz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Beolvasas
+            Console.Write("Hőmérséklet: ");
+            double homerseklet = Convert.ToDouble(Console.ReadLine());
+
+            // MO1
+            //if (homerseklet <= 0)
+            //{
+            //    Console.WriteLine("Szilárd!");
+            //}
+            //else if (homerseklet <= 100)
+            //{
+            //    Console.WriteLine("Folyadék!");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Gáz!");
+            //}
+
+            // MO2
+            //if (homerseklet <= 0) Console.WriteLine("Szilárd!");
+            //else if (homerseklet <= 100) Console.WriteLine("Folyadék!");
+            //else Console.WriteLine("Gáz!");
+
+            // MO3 - 2. Feldolgozas
+            string halmazallapot;
+            if (homerseklet <= 0) halmazallapot = "Szilárd!";
+            else if (homerseklet <= 100) halmazallapot = "Folyadék!";
+            else halmazallapot = "Gáz!";
+
+            // MO4 - ternary
+            //string halmazallapot;
+            //halmazallapot = (homerseklet <= 0) ? "Szilárd!" :
+            //                (homerseklet <= 100) ? "Folyadék!" :
+            //                "Gáz!";
+
+            // Kiiras
+            Console.WriteLine(halmazallapot);
+
+            Console.ReadKey();
+        }
+    }
+}
