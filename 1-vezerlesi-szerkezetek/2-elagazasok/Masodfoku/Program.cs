57e8eab7638fd1d858a78ea7e7024203ff988f12
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-16 14:47:21 +0200

    Masodfoku, Osztalyzat

---
+++ 1-vezerlesi-szerkezetek/2-elagazasok/Masodfoku/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Masodfoku
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c;
+
+            Console.Write("a: ");
+            a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b: ");
+            b = Convert.ToInt32(Console.ReadLine());
+            Console.Write("c: ");
+            c = Convert.ToInt32(Console.ReadLine());
+
+            int d = b * b - 4 * a * c;
+
+            if (d > 0)
+            {
+                Console.WriteLine("2 db megoldás");
+            }
+            else if (d < 0)
+            {
+                Console.WriteLine("Nincs valós megoldás");
+            }
+            else
+            {
+                Console.WriteLine("1 db megoldás");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1-vezerlesi-szerkezetek/2-elagazasok/Osztalyzat/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Osztalyzat
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+            int jegy = r.Next(1, 6);
+
+            // 1. if-else
+            if (jegy == 1) Console.WriteLine("Elégtelen!");
+            else if (jegy == 2) Console.WriteLine("Elégséges!");
+            else if (jegy == 3) Console.WriteLine("Közepes!");
+            else if (jegy == 4) Console.WriteLine("Jó!");
+            else Console.WriteLine("Jeles!");
+
+            // 2. switch
+            switch (jegy)
+            {
+                case 1:
+                    Console.WriteLine("Elégtelen!");
+                    break;
+                case 2:
+                    Console.WriteLine("Elégséges!");
+                    break;
+                case 3:
+                    Console.WriteLine("Közepes!");
+                    break;
+                case 4:
+                    Console.WriteLine("Jó!");
+                    break;
+                case 5:
+                    Console.WriteLine("Jeles!");
+                    break;
+            }
+
+            // 3. tömbök (?)
+
+            Console.ReadKey();
+        }
+    }
+}
