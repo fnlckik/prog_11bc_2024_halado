4ef31aaf563eb029dc18e7cbd33882640061393b
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-26 12:27:42 +0200

    Osztok, Kocka a)

---
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Kocka/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Kocka
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Dobások száma: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Veszélyes változónevek:
+            // i, l, I, 1
+            // o, O, 0
+
+            // a) Dobjunk n-szer a kockával
+            Random r = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                int aktualis = r.Next(1, 7);
+                Console.Write(aktualis + " ");
+            }
+
+            // b) Dobáljuk a kockát amíg 6-ost nem kapunk!
+
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Osztok/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Osztok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Deklaráció (hely foglalás a memóriában)
+            int n;
+            do
+            {
+                Console.Write("n: ");
+                n = Convert.ToInt32(Console.ReadLine());
+            } while (n <= 0);
+
+            // a) Osztók: 12 => 1 2 3 4 6 12
+            Console.WriteLine("Osztói: ");
+            for (int i = 1; i <= n/2; i++)
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+            Console.WriteLine(n);
+
+            Console.ReadKey();
+        }
+    }
+}
