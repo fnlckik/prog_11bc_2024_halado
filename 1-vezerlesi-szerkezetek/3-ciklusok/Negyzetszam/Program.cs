70f87e47b363f07d43e7cc1a1f28a309bfffba7c
farkas.norbert <farkas.norbert@ckik.hu>
2024-09-26 11:51:35 +0200

    Telefon, Negyzetszam (kesz)

---
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Negyzetszam/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Negyzetszam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("n: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // a) N darab négyzetszám
+            Console.WriteLine("a) feladat:");
+            for (int i = 0; i < n; i++)
+            {
+                //Console.Write($"{i*i} ");
+                Console.Write(i*i + " ");
+            }
+
+            // b) N-ig a négyzetszámok
+            Console.WriteLine("\nb) feladat:");
+            int j = 0;
+            while (j*j < n)
+            {
+                Console.Write(j*j + " ");
+                j++;
+            }
+            // Kevesebb memória ("hatékonyabb") => Lasabb futás
+
+            // c) N-ig a négyzetszámok
+            Console.WriteLine("\nc) feladat:");
+            int k = 0;
+            int negyzet = 0;
+            while (negyzet < n)
+            {
+                Console.Write(negyzet + " ");
+                k++;
+                negyzet = k * k;
+            }
+            // Kisebb futási idő ("hatékonyabb") => Több memória!
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Telefon/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Telefon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Konstans: fordítási időben tudnunk kell az értékét
+            // (és az nem változhat a program futása során)
+            const int n = 12;
+            // a) For ciklussal
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"{i+1}. Nem mobilozok órán!");
+            }
+
+            Console.WriteLine();
+
+            // b) While ciklussal
+            int j = 0;
+            while (j < n)
+            {
+                Console.WriteLine($"{j + 1}. Nem mobilozok órán!");
+                j++;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
