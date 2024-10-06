d2a18489640d1875c9c738a8c606e2235b55d296
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-06 17:00:40 +0200

    3-ciklusok Paratlan

---
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Paratlan/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Paratlan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random r = new Random();
+
+            const int n = 7;
+
+            string szam = "";
+            for (int i = 0; i < n; i++)
+            {
+                int szamjegy = r.Next(10); // 0, 1, 2, ... 9
+                if (szamjegy % 2 == 0)
+                {
+                    szamjegy++;
+                }
+                szam += szamjegy; // "1773" + 7
+                // Console.Write(szamjegy);
+            }
+
+            Console.Write($"A generált szám: {szam}");
+
+            Console.ReadKey();
+        }
+    }
+}
