b3cf14a7c6b0250ea492857fb6a942a7b8fe384b
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-07 12:31:17 +0200

    tombok -> Gyumolcsok

---
+++ 2-programozasi-tetelek/1-tombok/Gyumolcsok/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Gyumolcsok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] gyumolcsok = { "cigánymeggy", "görögdinnye", "paradicsom" };
+            Console.WriteLine("a) Egyesével:");
+            Console.WriteLine(gyumolcsok[0]);
+            Console.WriteLine(gyumolcsok[1]);
+            Console.WriteLine(gyumolcsok[2]);
+
+            Console.WriteLine();
+            Console.WriteLine("b) Ciklussal");
+            for (int i = 0; i < gyumolcsok.Length; i++)
+            {
+                Console.WriteLine(gyumolcsok[i]);
+            }
+
+            Console.WriteLine();
+            Console.WriteLine("c) Foreach (titkos tanok)");
+            foreach (string gy in gyumolcsok)
+            {
+                Console.WriteLine(gy);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
