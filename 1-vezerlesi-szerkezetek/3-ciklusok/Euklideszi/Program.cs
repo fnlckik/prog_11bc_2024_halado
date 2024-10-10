84c6f1e25a91d8bb67d819892c32c2ebe46d3038
farkas.norbert <farkas.norbert@ckik.hu>
2024-10-10 08:51:40 +0200

    Euklideszi

---
+++ 1-vezerlesi-szerkezetek/3-ciklusok/Euklideszi/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Euklideszi
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("a: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.Write("b: ");
+            int b = Convert.ToInt32(Console.ReadLine());
+
+            int eredetiA = a, eredetiB = b;
+
+            // Mj: a.ToString().PadLeft(4)
+            int r = a % b;
+            while (r != 0)
+            {
+                Console.WriteLine($"{a} = {a/b} * {b} + {r}");
+                a = b;
+                b = r;
+                r = a % b;
+            }
+            Console.WriteLine($"{a} = {a / b} * {b} + {r}");
+
+            Console.WriteLine($"({eredetiA}, {eredetiB}) = {b}");
+            Console.WriteLine($"[{eredetiA}, {eredetiB}] = {(eredetiA * eredetiB) / b}");
+
+            Console.ReadKey();
+        }
+    }
+}
