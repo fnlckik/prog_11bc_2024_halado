db255695d998e8d3e641ff53afbb9f0fdcc5b43d
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-20 09:40:37 +0100

    1-autoszerelo Ugyfel tabla (F2, F4)

--- IKT/Adatbazis/1-Autoszerelo/1-autoszerelo.sql
+++ IKT/Adatbazis/1-Autoszerelo/1-autoszerelo.sql
@@ -1,13 +1,33 @@
 -- F1
 -- Futtatás: CTRL + Enter
-CREATE DATABASE IF NOT EXISTS Autoszerelo;
+DROP DATABASE IF EXISTS Autoszerelo;
+CREATE DATABASE Autoszerelo;
 USE Autoszerelo;
 
 -- F2
-CREATE TABLE IF NOT EXISTS Ugyfel (
+DROP TABLE IF EXISTS Ugyfel;
+CREATE TABLE Ugyfel (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    nev VARCHAR(50),
-    email VARCHAR(100),
+    nev VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
     telefon CHAR(11),
-    aktiv BOOLEAN
+    aktiv BOOLEAN DEFAULT TRUE
 );
+
+-- Kitekintés:
+/*
+INSERT INTO Ugyfel (nev, email, telefon, aktiv)
+VALUES
+("Kis Pista", "kis.pista@gmail.com", "06202235123", TRUE),
+("Fekete Laci", NULL, NULL, FALSE);
+*/
+
+-- F4
+INSERT INTO Ugyfel (nev, email, telefon, aktiv)
+VALUES
+('Kovács Péter', 'kovacs.peter@example.com', '36201234567', TRUE),
+('Nagy Anna', 'nagy.anna@example.com', '36301122334', TRUE),
+('Szabó László', 'szabo.laszlo@example.com', NULL, TRUE),
+('Tóth Mária', 'toth.maria@example.com', NULL, FALSE),
+('Varga István', 'varga.istvan@example.com', '36201111222', TRUE);
+
