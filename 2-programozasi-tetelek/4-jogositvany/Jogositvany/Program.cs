38f38a41478989837a368e855c7eaeaa16b5442d
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-20 13:25:54 +0100

    4-jogositvany STATIKUS TÖMB

---
+++ 2-programozasi-tetelek/4-jogositvany/Jogositvany/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Jogositvany
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+
+            // Statikus tömbök
+            // - tömbök méretei fixek
+            // - fordítási időben tudni kell a méretüket (!!!)
+            string[] nevek = new string[100];
+
+            Console.ReadKey();
+        }
+    }
+}
