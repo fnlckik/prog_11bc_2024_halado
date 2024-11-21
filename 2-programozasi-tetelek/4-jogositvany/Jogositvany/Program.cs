5a5de410403cf0489d96ed87cc8cdedd417a5cd6
farkas.norbert <farkas.norbert@ckik.hu>
2024-11-21 12:29:06 +0100

    Jogositvany .Split(' ')

--- 2-programozasi-tetelek/4-jogositvany/Jogositvany/Program.cs
+++ 2-programozasi-tetelek/4-jogositvany/Jogositvany/Program.cs
@@ -6,14 +6,27 @@ namespace Jogositvany
     {
         static void Main(string[] args)
         {
+            // Elnevezési konvenciók
+            // PascalCase - C# (ReadLine)
+            // camelCase - JS (addEventListener)
             int n = Convert.ToInt32(Console.ReadLine());
 
             // Statikus tömbök
             // - tömbök méretei fixek
             // - fordítási időben tudni kell a méretüket (!!!)
             string[] nevek = new string[100];
+            int[] korok = new int[100];
+            bool[] jogsik = new bool[100];
 
-            Console.ReadKey();
+            int i;
+            for (i = 0; i < n; i++)
+            {
+                string sor = Console.ReadLine();
+                string[] adatok = sor.Split(' ');
+                Console.WriteLine(adatok);
+            }
+
+            //Console.ReadKey();
         }
     }
 }
