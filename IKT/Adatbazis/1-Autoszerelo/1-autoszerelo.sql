de036d341cea530bd78f1580907660cec0586637
fnlckik <farkas.norbert@ckik.hu>
2024-11-27 13:07:40 +0100

    1-autoszerelo.sql (INSERT INTO Javitas)

--- IKT/Adatbazis/1-Autoszerelo/1-autoszerelo.sql
+++ IKT/Adatbazis/1-Autoszerelo/1-autoszerelo.sql
@@ -36,11 +36,13 @@ CREATE TABLE Javitas (
     leiras TEXT,
     FOREIGN KEY (ugyfel_id) REFERENCES Ugyfel(id),
     CONSTRAINT UQ_Javitas_ugyfelId_rendszam_datum UNIQUE (ugyfel_id, rendszam, datum),
-    CONSTRAINT CHK_Javitas_koltseg CHECK (12000 <= koltseg && koltseg <= 500000)
+    CONSTRAINT CHK_Javitas_koltseg CHECK (koltseg BETWEEN 12000 AND 500000)
 );
 -- Másképp:
 -- CHECK (12000 <= koltseg AND koltseg <= 500000)
 -- CHECK (12000 <= koltseg && koltseg <= 500000)
+-- CHECK (koltseg BETWEEN 12000 AND 500000)
+-- 12000 és 500000 közötti (határokat is beleértve)
 
 -- Példa:
 /*
@@ -60,3 +62,18 @@ VALUES
 ('Tóth Mária', 'toth.maria@example.com', NULL, FALSE),
 ('Varga István', 'varga.istvan@example.com', '36201111222', TRUE);
 
+INSERT INTO Javitas (ugyfel_id, rendszam, datum, koltseg, leiras)
+VALUES 
+(3, 'LMN123', '2024-04-10', 30000, 'Hűtőrendszer javítása'),
+(1, 'DEF456', '2024-03-20', 20000, 'Szélvédő javítás'),
+(4, 'QRS456', '2024-05-12', 12000, 'Olajcsere'),
+(2, 'XYZ789', '2024-01-20', 80000, 'Motorhiba javítása'),
+(5, 'TUV789', '2024-03-05', 500000, 'Komplex felújítás'),
+(1, 'ABC123', '2024-02-15', 15000, 'Téli gumik felszerelése'),
+(4, 'QRS456', '2024-06-01', NULL, 'Kormánymű javítása'),
+(1, 'ABC123', '2024-01-10', 45000, 'Olajcsere és szűrők cseréje'),
+(2, 'XYZ789', '2024-02-05', 20000, 'Kipufogó csere'),
+(5, 'TUV789', '2024-02-25', 250000, 'Teljes karosszéria lakatolás'),
+(3, 'LMN123', '2024-03-15', NULL, 'Akkumulátor csere'),
+(1, 'DEF456', '2024-03-01', NULL, 'Fékek teljes cseréje'),
+(2, 'XYZ789', '2024-04-15', 15000, 'Gumiabroncs javítás');
