7c843feff3fa1bae559c4cb342f2e87cb19930d8
fnlckik <farkas.norbert@ckik.hu>
2024-12-06 14:29:30 +0100

    Primszamok, OsztokSzama

--- 3-fuggvenyek/Szamelmelet/Program.cs
+++ 3-fuggvenyek/Szamelmelet/Program.cs
@@ -6,10 +6,8 @@ namespace Szamelmelet
     {
         static bool PrimE(int p)
         {
-            if (p < 2)
-            {
-                return false;
-            }
+            if (p < 2) return false;
+            if (p == 2) return true;
             int i = 2;
             while (i < Math.Sqrt(p) && p % i != 0)
             {
@@ -18,12 +16,54 @@ namespace Szamelmelet
             return p % i != 0; // Prím: kiléptünk, mert nincs osztó
         }
 
+        static int[] Primszamok(int[] x, ref int db)
+        {
+            int[] primek = new int[100];
+            for (int i = 0; i < x.Length; i++)
+            {
+                if (PrimE(x[i]))
+                {
+                    primek[db] = x[i];
+                    db++;
+                }
+            }
+            return primek;
+        }
+
+        static void Kiir(int[] primek, int db)
+        {
+            Console.Write("Prímszámok: ");
+            for (int i = 0; i < db; i++)
+            {
+                Console.Write(primek[i] + " ");
+            }
+        }
+
+        static int OsztokSzama(int n)
+        {
+            int db = 1; // önmaga osztója biztosan
+            for (int i = 1; i <= n/2; i++)
+            {
+                if (n % i == 0)
+                {
+                    db++;
+                }
+            }
+            return db;
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine(PrimE(17)); // true
-            Console.WriteLine(PrimE(24)); // false
-            Console.WriteLine(PrimE(9)); // false 
-            Console.WriteLine(PrimE(1));
+            // Prímszámok kiválogatása - Kiválogatás + Eldöntés
+            int[] szamok = { 7, 23, 6, 42, 73, 2, 3, 9, 1, 5 };
+            int[] primek = new int[100];
+            int db = 0;
+            primek = Primszamok(szamok, ref db);
+            Kiir(primek, db);
+            Console.WriteLine();
+
+            // Sok osztós szám - Keresés + Megszámolás
+            Console.WriteLine(OsztokSzama(1));
         }
     }
 }
