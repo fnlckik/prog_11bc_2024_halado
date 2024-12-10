866111529af939989ff7d2b4691f85e9bc6ea2e2
fnlckik <farkas.norbert@ckik.hu>
2024-12-10 12:24:39 +0100

    4-Kivalogatas-2-Allatok

---
+++ Mester/Kezdo/4-Kivalogatas-2-Allatok/Allatok/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Allatok
+{
+    internal class Program
+    {
+        static void Beolvas(int n, string[] x, string[] y)
+        {
+            // ???
+        }
+
+        static void Main(string[] args)
+        {
+            int n = Convert.ToInt32(Console.ReadLine());
+            string[] evok = new string[30];
+            string[] evettek = new string[30];
+            Beolvas(n, evok, evettek);
+        }
+    }
+}
