2eb4045dcdf441fae2692d44f9645bf0db2acf9f
fnlckik <farkas.norbert@ckik.hu>
2024-12-11 09:41:17 +0100

    2-Allatok megoldás

--- Mester/Kezdo/4-Kivalogatas-2-Allatok/Allatok/Program.cs
+++ Mester/Kezdo/4-Kivalogatas-2-Allatok/Allatok/Program.cs
@@ -4,17 +4,61 @@ namespace Allatok
 {
     internal class Program
     {
-        static void Beolvas(int n, string[] x, string[] y)
+        static void Beolvas(out int n, string[] x, string[] y)
         {
-            // ???
+            n = Convert.ToInt32(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                string[] sor = Console.ReadLine().Split(' ');
+                x[i] = sor[0];
+                y[i] = sor[1];
+            }
+        }
+
+        static bool Eleme(string elem, string[] tomb)
+        {
+            int i = 0;
+            while (i < tomb.Length && tomb[i] != elem)
+            {
+                i++;
+            }
+            return i < tomb.Length;
+        }
+
+        static void Kivalogatas(string[] evok, string[] evettek, int n, string[] allatok, out int db)
+        {
+            db = 0;
+            for (int i = 0; i < n; i++)
+            {
+                // Ha jobb oldali benne van a bal oldaliban
+                // Ha még nem válogattuk ki (bal oldali nincs benne a kiválogatottakban)
+                if (Eleme(evettek[i], evok) && !Eleme(evok[i], allatok))
+                {
+                    allatok[db] = evok[i]; // kivesszük a balt
+                    db++;
+                }
+            }
+        }
+
+        static void Kiir(string[] x, int db)
+        {
+            Console.WriteLine(db);
+            for (int i = 0; i < db; i++)
+            {
+                Console.WriteLine(x[i]);
+            }
         }
 
         static void Main(string[] args)
         {
-            int n = Convert.ToInt32(Console.ReadLine());
             string[] evok = new string[30];
             string[] evettek = new string[30];
-            Beolvas(n, evok, evettek);
+            Beolvas(out int n, evok, evettek);
+
+            string[] allatok = new string[30];
+            Kivalogatas(evok, evettek, n, allatok, out int db);
+
+            Kiir(allatok, db);
         }
     }
 }
