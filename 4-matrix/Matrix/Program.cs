e9b5abb509b20a7c444dc7a5d05b46f54cff37f9
fnlckik <farkas.norbert@ckik.hu>
2024-12-13 14:17:54 +0100

    Mester - ták-ták-ták

--- 4-matrix/Matrix/Program.cs
+++ 4-matrix/Matrix/Program.cs
@@ -20,8 +20,8 @@ namespace Matrix
 
             // F2
             //Console.WriteLine(matrix.Length);
-            int n = matrix.GetLength(0);
-            int m = matrix.GetLength(1);
+            int n = matrix.GetLength(0); // sorok száma
+            int m = matrix.GetLength(1); // oszlopok száma
             for (int i = 0; i < n; i++)
             {
                 for (int j = 0; j < m; j++)
@@ -41,6 +41,48 @@ namespace Matrix
                 }
             }
             Console.WriteLine("Elemek összege: " + s);
+
+            // F4
+            int maxi = 0, maxj = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j] > matrix[maxi, maxj])
+                    {
+                        maxi = i;
+                        maxj = j;
+                    }
+                }
+            }
+            // ertek = matrix[maxi, maxj]
+            Console.WriteLine($"Maximális elem helye: {maxi+1}, {maxj+1}");
+
+            // F5 - a
+            Console.Write("Soronként összeg: ");
+            for (int i = 0; i < n; i++)
+            {
+                int sorosszeg = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    sorosszeg += matrix[i, j];
+                }
+                Console.Write(sorosszeg + " ");
+            }
+            Console.WriteLine();
+
+            // F5 - b
+            Console.Write("Oszloponként összeg: ");
+            for (int i = 0; i < m; i++) // i: oszlop
+            {
+                int oszloposszeg = 0;
+                for (int j = 0; j < n; j++) // j: sor
+                {
+                    oszloposszeg += matrix[j, i];
+                }
+                Console.Write(oszloposszeg + " ");
+            }
+            Console.WriteLine();
         }
     }
 }
---
+++ Mester/Kozephalado/1-Idojaras-9-Legmelegebb/Meleg/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Meleg
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, m;
+            string[] elsosor = Console.ReadLine().Split(' ');
+            n = Convert.ToInt32(elsosor[0]);
+            m = Convert.ToInt32(elsosor[1]);
+
+            int[,] h = new int[1000, 1000];
+            // HF: mátrixot beolvasni
+        }
+    }
+}
