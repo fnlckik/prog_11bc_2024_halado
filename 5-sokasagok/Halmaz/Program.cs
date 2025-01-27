025b45add7d5ca99171878097629551d9b0bb721
farkas.norbert <farkas.norbert@ckik.hu>
2025-01-27 12:06:28 +0100

    Halmaz

---
+++ 5-sokasagok/Halmaz/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Halmaz
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Halmaz: elemei egyediek, sorrend nincs => nem indexelhető
+            HashSet<int> halmaz = new HashSet<int> { 5, 2, 9, 2, 2, 5, 7 }; // 5 2 9 7
+            Console.WriteLine("Halmaz elemszáma: " + halmaz.Count);
+
+            // Ilyet nem lehet!
+            //Console.WriteLine("1. elem: " + halmaz[0]);
+
+            Console.WriteLine("Eleme-e a 2 szám? " + halmaz.Contains(2));
+
+            halmaz.Add(13); // 5 2 9 7 13
+            halmaz.Remove(7); // 5 2 9 13
+            halmaz.Add(13);
+            halmaz.Remove(7);
+            Console.Write("Halmaz elemei: ");
+            Kiir(halmaz);
+
+            Console.WriteLine();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine();
+
+            HashSet<string> zrinyi = new HashSet<string> { "Bence", "Réka", "Máté", "Pisti", "Johanna", "Kata" };
+            HashSet<string> oktv = new HashSet<string> { "Máté", "Zalán", "Csaba", "Kata", "Bence" };
+        }
+
+        static void Kiir(HashSet<int> halmaz)
+        {
+            foreach (int elem in halmaz)
+            {
+                Console.Write(elem + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
--- Mester/Kozephalado/7-Verseny-17-Nehezek/Nehezek/Program.cs
+++ Mester/Kozephalado/7-Verseny-17-Nehezek/Nehezek/Program.cs
@@ -8,11 +8,45 @@ namespace Nehezek
         static void Main(string[] args)
         {
             List<int> minek = new List<int>();
-            List<List<int>> pontok = new List<List<int>>();
-            Beolvas(minek, pontok);
+            List<List<int>> versenyek = new List<List<int>>();
+            // versenyek[i][j]: i. versenyen indult j. tanuló pontszáma
+            Beolvas(minek, versenyek);
+            List<int> nehezek = Nehezek(minek, versenyek);
+            Kiir(nehezek);
         }
 
-        static void Beolvas(List<int> minek, List<List<int>> pontok)
+        static void Kiir(List<int> nehezek)
+        {
+            Console.Write(nehezek.Count + " ");
+            foreach (int sorszam in nehezek)
+            {
+                Console.Write(sorszam + " ");
+            }
+            Console.WriteLine();
+        }
+
+        static List<int> Nehezek(List<int> minek, List<List<int>> versenyek)
+        {
+            List<int> nehezek = new List<int>();
+            for (int i = 0; i < versenyek.Count; i++)
+            {
+                int db = 0;
+                foreach (int pont in versenyek[i])
+                {
+                    if (pont < minek[i])
+                    {
+                        db++;
+                    }
+                }
+                if (db >= (double)versenyek[i].Count / 2)
+                {
+                    nehezek.Add(i + 1);
+                }
+            }
+            return nehezek;
+        }
+
+        static void Beolvas(List<int> minek, List<List<int>> versenyek)
         {
             string[] sor = Console.ReadLine().Split(' ');
             int n = int.Parse(sor[1]); // versenyek száma
@@ -28,7 +62,12 @@ namespace Nehezek
             for (int i = 0; i < n; i++)
             {
                 sor = Console.ReadLine().Split(' ');
-                // ???
+                List<int> pontok = new List<int>();
+                for (int j = 2; j < sor.Length; j += 2)
+                {
+                    pontok.Add(int.Parse(sor[j]));
+                }
+                versenyek.Add(pontok);
             }
         }
     }
