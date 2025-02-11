672b526d78ac2513be189b81f0e88ae46daee0a1
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-11 12:22:13 +0100

    Fajlok - StreamReader

---
+++ 6-rendezes-fajlok/Fajlok/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Fajlok
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> kedvencek = new Dictionary<string, int>();
+            Olvas(kedvencek);
+        }
+
+        static void Olvas(Dictionary<string, int> kedvencek)
+        {
+            //StreamReader reader = new StreamReader("../../primek.txt");
+            StreamReader reader = new StreamReader("primek.txt");
+            reader.Close();
+        }
+    }
+}
