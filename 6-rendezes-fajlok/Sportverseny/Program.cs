a0f195eeb55b6d366ca4add2aa9db52b9dc65914
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-14 13:42:10 +0100

    Sportverseny - Beolvas, Kiir

--- 6-rendezes-fajlok/Sportverseny/Program.cs
+++ 6-rendezes-fajlok/Sportverseny/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 
 namespace Sportverseny
 {
@@ -7,7 +8,34 @@ namespace Sportverseny
     {
         static void Main(string[] args)
         {
+            List<string> nevek = new List<string>();
+            List<string> orszagok = new List<string>();
+            List<int> pontok = new List<int>();
+            Beolvas(nevek, orszagok, pontok);
+            Kiir(nevek, orszagok, pontok);
+        }
 
+        static void Kiir(List<string> nevek, List<string> orszagok, List<int> pontok)
+        {
+            StreamWriter writer = new StreamWriter("minta.txt");
+            for (int i = 0; i < nevek.Count; i++)
+            {
+                writer.WriteLine($"{i+1}. {nevek[i]} ({orszagok[i]}) => {pontok[i]}");
+            }
+            writer.Close();
+        }
+
+        static void Beolvas(List<string> nevek, List<string> orszagok, List<int> pontok)
+        {
+            StreamReader reader = new StreamReader("eredmeny.csv");
+            while (!reader.EndOfStream)
+            {
+                string[] sor = reader.ReadLine().Split(',');
+                nevek.Add(sor[0]);
+                orszagok.Add(sor[1]);
+                pontok.Add(int.Parse(sor[2]));
+            }
+            reader.Close();
         }
     }
 }
