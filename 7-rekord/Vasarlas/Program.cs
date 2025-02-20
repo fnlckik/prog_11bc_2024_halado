263a21ea1fc5e0011ccdbfdaf57e79e9489ce98b
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-20 11:52:58 +0100

    Vásárlás - termekek.txt

---
+++ 7-rekord/Vasarlas/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Vasarlas
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
