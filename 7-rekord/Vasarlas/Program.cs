3d6b83d43fa9f20b5d55f40882ad3a71640d1ff6
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-24 12:26:23 +0100

    Vasarlas - Rendezés

--- 7-rekord/Vasarlas/Program.cs
+++ 7-rekord/Vasarlas/Program.cs
@@ -21,15 +21,117 @@ namespace Vasarlas
             //Kiir(termekek);
             Kiir2(termekek);
 
+            Console.WriteLine();
             // Mennyit fizettünk a boltban? (összegzés)
             Console.WriteLine("Teljes ár: " + Osszeg2(termekek));
 
             // Mindegyiket "klasszikus módon" (for, while) és foreach ciklussal is!
-            // Melyik a legdrágább termék?
+            // Melyik a legdrágább termék? => nev
+            Console.WriteLine("Legdrágább termék: " + Legdragabb2(termekek).nev);
 
             // Adj meg egy olyan terméket, aminek az ára 3000 Ft feletti!
+            Console.WriteLine("3000 Ft feletti termék: " + DragaTermek2(termekek));
 
             // Válogasd ki egy listába az élelmiszereket!
+            List<Termek> elelmiszerek = new List<Termek>();
+            Kivalogat(termekek, elelmiszerek);
+            Console.WriteLine();
+            Kiir(elelmiszerek);
+
+            // Rendezzük ár szerint növekvő sorrendbe a termékeket!
+            //Rendez(termekek);
+            termekek.Sort();
+
+            Console.WriteLine();
+            Kiir(termekek);
+        }
+
+        // 5 1 2 6 1 1 2
+        static void Rendez(List<Termek> termekek)
+        {
+            for (int i = 0; i < termekek.Count; i++)
+            {
+                int mini = i;
+                for (int j = i + 1; j < termekek.Count; j++)
+                {
+                    if (termekek[j].ar < termekek[mini].ar)
+                    {
+                        mini = j;
+                    }
+                }
+                Csere(termekek, i, mini);
+            }
+        }
+
+        static void Csere(List<Termek> termekek, int i, int k)
+        {
+            (termekek[i], termekek[k]) = (termekek[k], termekek[i]);
+        }
+
+        static void Kivalogat(List<Termek> termekek, List<Termek> elelmiszerek)
+        {
+            foreach (Termek termek in termekek)
+            {
+                if (termek.elelmiszerE)
+                {
+                    elelmiszerek.Add(termek);
+                }
+            }
+        }
+
+        static string DragaTermek2(List<Termek> termekek)
+        {
+            foreach (Termek termek in termekek)
+            {
+                if (termek.ar >= 3000)
+                {
+                    return termek.nev;
+                }
+            }
+            return "Nincs ilyen!";
+        }
+
+        static string DragaTermek(List<Termek> termekek)
+        {
+            int i = 0;
+            while (i < termekek.Count && !(termekek[i].ar >= 3000))
+            {
+                i++;
+            }
+            if (i < termekek.Count)
+            {
+                return termekek[i].nev;
+            }
+            else
+            {
+                return "Nincs ilyen!";
+            }
+        }
+
+        static Termek Legdragabb2(List<Termek> termekek)
+        {
+            int maxi = 0; // Maximális árú termék indexe!
+            for (int i = 0; i < termekek.Count; i++)
+            {
+                if (termekek[i].ar > termekek[maxi].ar)
+                {
+                    maxi = i;
+                }
+            }
+            return termekek[maxi];
+        }
+
+        static Termek Legdragabb(List<Termek> termekek)
+        {
+            Termek maxTermek = termekek[0];
+            foreach (Termek termek in termekek)
+            {
+                if (termek.ar > maxTermek.ar)
+                {
+                    maxTermek = termek;
+                }
+            }
+            return maxTermek;
         }
 
         static int Osszeg2(List<Termek> termekek)
