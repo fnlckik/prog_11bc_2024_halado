2e0288d875ff99c989a31bc4c67e90deb7183f05
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-25 12:25:00 +0100

    Epitmenyado - F1

---
+++ Erettsegi/2022-maj-info_e-Epitemenyado/Epitmenyado/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Epitmenyado
+{
+    internal class Program
+    {
+        struct Epulet
+        {
+            public string adoszam;
+            public string utca;
+            public string hsz;
+            public char tipus; // adó típusa
+            public int terulet;
+        }
+
+        static void Main(string[] args)
+        {
+            List<Epulet> epuletek = new List<Epulet>();
+            Dictionary<char, int> adok = new Dictionary<char, int>();
+            Beolvas(epuletek, adok);
+        }
+
+        static void Beolvas(List<Epulet> epuletek, Dictionary<char, int> adok)
+        {
+            StreamReader sr = new StreamReader("utca.txt");
+            string[] sor = sr.ReadLine().Split(' '); // { "800", "600", "100" }
+            adok.Add('A', int.Parse(sor[0]));
+            adok.Add('B', int.Parse(sor[1]));
+            adok.Add('C', int.Parse(sor[2]));
+            while (!sr.EndOfStream)
+            {
+                sor = sr.ReadLine().Split(' ');
+                Epulet epulet;
+                epulet.adoszam = sor[0];
+                epulet.utca = sor[1];
+                epulet.hsz = sor[2];
+                epulet.tipus = char.Parse(sor[3]);
+                epulet.terulet = int.Parse(sor[4]);
+                epuletek.Add(epulet);
+            }
+            sr.Close();
+        }
+    }
+}
---
+++ Erettsegi/2022-maj-info_e-Epitemenyado/Epitmenyado/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Epitmenyado")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Epitmenyado")]
+[assembly: AssemblyCopyright("Copyright ©  2025")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("d5e357d8-1c97-4061-a714-6bb863a45b9d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
