6e7878f8ef8c07332f36d6effa91a8f6d258cde2
farkas.norbert <farkas.norbert@ckik.hu>
2025-02-26 09:41:20 +0100

    Építményadó - F2-F4

--- Erettsegi/2022-maj-info_e-Epitemenyado/Epitmenyado/Program.cs
+++ Erettsegi/2022-maj-info_e-Epitemenyado/Epitmenyado/Program.cs
@@ -20,6 +20,56 @@ namespace Epitmenyado
             List<Epulet> epuletek = new List<Epulet>();
             Dictionary<char, int> adok = new Dictionary<char, int>();
             Beolvas(epuletek, adok);
+            F2(epuletek);
+            //F3(epuletek);
+            Console.WriteLine(Ado('C', 180, adok));
+        }
+
+        // 'C', 180 + Dictionary
+        static int Ado(char adosav, int alapterulet, Dictionary<char, int> adok)
+        {
+            //int ado;
+            //if (adosav == 'A')
+            //{
+            //    ado = adok['A'] * alapterulet;
+            //}
+            //else if (adosav == 'B')
+            //{
+            //    ado = adok['B'] * alapterulet;
+            //}
+            //else
+            //{
+            //    ado = adok['C'] * alapterulet;
+            //}
+            //if (ado < 10000) ado = 0;
+            //return ado;
+            int ado = adok[adosav] * alapterulet;
+            return ado >= 10000 ? ado : 0;
+        }
+
+        static void F3(List<Epulet> epuletek)
+        {
+            // 68396
+            Console.Write("3. feladat. Egy tulajdonos adószáma: ");
+            string adoszam = Console.ReadLine();
+            int db = 0;
+            foreach (Epulet epulet in epuletek)
+            {
+                if (epulet.adoszam == adoszam)
+                {
+                    Console.WriteLine($"{epulet.utca} utca {epulet.hsz}");
+                    db++;
+                }
+            }
+            if (db == 0)
+            {
+                Console.WriteLine("Nem szerepel az adatállományban.");
+            }
+        }
+
+        static void F2(List<Epulet> epuletek)
+        {
+            Console.WriteLine($"2. feladat. A mintában {epuletek.Count} telek szerepel.");
         }
 
         static void Beolvas(List<Epulet> epuletek, Dictionary<char, int> adok)
