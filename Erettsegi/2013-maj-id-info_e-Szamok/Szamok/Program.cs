79a2be76ec9309f3d3f656abec51d28747db4656
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-11 12:28:24 +0100

    Szamok (üres projekt)

---
+++ Erettsegi/2013-maj-id-info_e-Szamok/Szamok/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Szamok
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
