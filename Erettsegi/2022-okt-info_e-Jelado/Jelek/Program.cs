da270132540aeb47947f9e072f6e26950323b7c3
farkas.norbert <farkas.norbert@ckik.hu>
2025-03-31 12:26:06 +0200

    TimeSpan

---
+++ Erettsegi/2022-okt-info_e-Jelado/Jelek/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jelek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //DateTime d = new DateTime(2025, 03, 31, 12, 23, 41);
+            TimeSpan t = new TimeSpan(12, 23, 41);
+            Console.WriteLine(t);
+        }
+    }
+}
