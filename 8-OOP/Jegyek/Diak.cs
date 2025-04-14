1de2d2be270f875decb48d9e6e6a79abd6c6eb94
farkas.norbert <farkas.norbert@ckik.hu>
2025-04-14 12:27:54 +0200

    Diak.cs - fields, constructor, methods, override

---
+++ 8-OOP/Jegyek/Diak.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Jegyek
+{
+    // Cél: a világ modellezése
+    // Osztály: "sablon, ahogyan valami működik"
+    // Objektum: az osztály egy konkrét példánya
+
+    // Osztály: egységbe zárja a
+    // 1. Adattagokat (field) => mező
+    // 2. Műveletek (method) => metódus
+    class Diak
+    {
+        public string nev;
+        public int kor;
+        public double hangulat; // 0.00 - 1.00
+
+        // function overloading (túlterhelés)
+        public Diak() { }
+
+        // this: osztály aktuális példánya
+        public Diak(string nev, int kor, double hangulat)
+        {
+            this.nev = nev;
+            this.kor = kor;
+            this.hangulat = hangulat;
+            Console.WriteLine("Létrejött a diák!");
+        }
+
+        public string Koszon()
+        {
+            return $"{nev} vagyok, {kor} éves.";
+        }
+
+        public override string ToString()
+        {
+            return $"{nev} vagyok, {kor} éves.";
+        }
+    }
+}
---
+++ 8-OOP/Jegyek/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+
+namespace Jegyek
+{
+    // Mj.: A struct és a class nagyon hasonlóak!
+    struct Tanulo
+    {
+        public string nev;
+        public int kor;
+        public double hangulat; // 0.00 - 1.00
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Korabban();
+
+            // Példányosítás: adott osztályba tartozó objektum létrehozása
+            // Konstruktor: az a függvény, ami példányosítás során lefut
+            Diak adel = new Diak();
+            adel.nev = "Adél";
+            adel.kor = 17;
+            adel.hangulat = 0.83;
+
+            Diak bela = new Diak("Béla", 15, 0.42);
+            Diak csaba = new Diak("Csaba", 17, 0.13);
+            Console.WriteLine();
+
+            // Bemutatkozás
+            // Procedurális - Koszon(adel)
+            // Koszon metódus - adel.Koszon()
+            Console.WriteLine(adel);
+            Console.WriteLine(bela);
+            Console.WriteLine(csaba);
+        }
+
+        //static string Koszon(Diak d)
+        //{
+        //    return $"{d.nev} vagyok, {d.kor} éves.";
+        //}
+
+        static void Korabban()
+        {
+            Tanulo adel;
+            adel.nev = "Adél";
+            adel.kor = 17;
+            adel.hangulat = 0.83;
+
+            Tanulo bela;
+            bela.nev = "Béla";
+            bela.kor = 15;
+            bela.hangulat = 0.42;
+
+            Tanulo csaba;
+            csaba.nev = "Csaba";
+            csaba.kor = 17;
+            csaba.hangulat = 0.13;
+
+            Console.WriteLine($"{adel.nev} {bela.nev} {csaba.nev}");
+        }
+    }
+}
