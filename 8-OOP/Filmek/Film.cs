8ac583c17587249eb14b321729be5dccad44646e
farkas.norbert <farkas.norbert@ckik.hu>
2025-05-12 08:47:55 +0200

    Filmek - feladat.txt, Film.cs .Cim .Ev

---
+++ 8-OOP/Filmek/Film.cs
@@ -0,0 +1,37 @@
+﻿namespace Filmek
+{
+    internal class Film
+    {
+        private string cim;
+        private int ev;
+        private string mufaj;
+        private double imdb;
+        private int nezok;
+
+        public Film(string cim, int ev, string mufaj, double imdb, int nezok)
+        {
+            this.cim = cim;
+            this.ev = ev;
+            this.mufaj = mufaj;
+            this.imdb = imdb;
+            this.nezok = nezok;
+        }
+
+        public string Cim { get => this.cim; }
+
+        public string Ev
+        { 
+            get
+            {
+                if (this.ev < 2019)
+                {
+                    return $"{this.ev} BC";
+                }
+                else
+                {
+                    return $"{this.ev} AC";
+                }
+            }
+        }
+    }
+}
---
+++ 8-OOP/Filmek/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Filmek
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // cim, ev, mufaj, imdb, nezok (akik értékelték imdb-n)
+            Film f1 = new Film("Wonder Woman", 2017, "akcio", 4.4, 30);
+            Film f2 = new Film("Vasember", 2008, "akcio", 7.6, 50);
+
+            Console.WriteLine($"{f1.Cim}: {f1.Ev}"); // Wonder Woman: 2017 BC
+        }
+    }
+}
