4497e3596a4cc94e24b19cbde36500c652362567
farkas.norbert <farkas.norbert@ckik.hu>
2025-05-13 07:58:44 +0200

    Film.cs getter, setter, methods

--- 8-OOP/Filmek/Film.cs
+++ 8-OOP/Filmek/Film.cs
@@ -1,4 +1,7 @@
-﻿namespace Filmek
+﻿
+using System.Collections.Generic;
+
+namespace Filmek
 {
     internal class Film
     {
@@ -7,6 +10,8 @@
         private string mufaj;
         private double imdb;
         private int nezok;
+
+        //private HashSet<string> mufajok = new HashSet<string> { "akcio", "fantasy", "kaland", "horror" };
 
         public Film(string cim, int ev, string mufaj, double imdb, int nezok)
         {
@@ -17,21 +22,42 @@
             this.nezok = nezok;
         }
 
+        public Film(string s)
+        {
+            string[] sor = s.Split(';');
+            new Film(sor[0], int.Parse(sor[1]), sor[2], double.Parse(sor[3]), int.Parse(sor[4]));
+        }
+
         public string Cim { get => this.cim; }
 
         public string Ev
         { 
             get
             {
-                if (this.ev < 2019)
-                {
-                    return $"{this.ev} BC";
-                }
-                else
-                {
-                    return $"{this.ev} AC";
-                }
+                if (this.ev < 2019) return $"{this.ev} BC";
+                else return $"{this.ev} AC";
             }
+        }
+
+        public string Mufaj
+        {
+            set
+            {
+                HashSet<string> mufajok = new HashSet<string> { "akcio", "fantasy", "kaland", "horror" };
+                if (!mufajok.Contains(value)) return; // garbage collector
+                this.mufaj = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"{this.cim} ({this.Ev}, {this.mufaj}) - {this.imdb}";
+        }
+
+        // Korábbi-e az aktuális film a paraméterként kapottnál?
+        public bool KorabbiE(Film film)
+        {
+            return this.ev < film.ev;
         }
     }
 }
--- 8-OOP/Filmek/Program.cs
+++ 8-OOP/Filmek/Program.cs
@@ -8,9 +8,19 @@ namespace Filmek
         {
             // cim, ev, mufaj, imdb, nezok (akik értékelték imdb-n)
             Film f1 = new Film("Wonder Woman", 2017, "akcio", 4.4, 30);
-            Film f2 = new Film("Vasember", 2008, "akcio", 7.6, 50);
+            //Film f2 = new Film("Vasember", 2008, "akcio", 7.6, 50);
+            Film f2 = new Film("Vasember;2008;akcio;7,6;50");
 
+            //Console.WriteLine(f2.Mufaj); // itt használjuk f2-t utoljára
+            // Garbage collector (szemétgyűjtő)
             Console.WriteLine($"{f1.Cim}: {f1.Ev}"); // Wonder Woman: 2017 BC
+
+            f1.Mufaj = "fantasy";
+            f1.Mufaj = "romantikus";
+            //Console.WriteLine(f1.Mufaj);
+            Console.WriteLine(f1);
+            Console.WriteLine(f2);
+            Console.WriteLine($"{f2.Cim} < {f1.Cim} :: {f2.KorabbiE(f1)}");
         }
     }
 }
