09434563a7520bdda34ffdadf8fec3fb9e6f5e64
farkas.norbert <farkas.norbert@ckik.hu>
2025-05-14 11:53:24 +0200

    Orszagok projekt

---
+++ 8-OOP/Orszagok/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Orszagok
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Ország:
+            // - nev
+            // - nepesseg (millió főben) => 9.730530
+            // - terulet (km^2)
+            // - eutagE (logikai)
+            // - nyelvek (halmaz)
+            Orszag hu = new Orszag("Magyarország", 9730530, 93000, "magyar angol német francia");
+            Orszag jp = new Orszag("Japán", 123294513, 4380000, "japán angol kínai koreai");
+
+            hu.EutagE = true;
+            jp.EutagE = true;
+            
+            Console.WriteLine(hu);
+            Console.WriteLine(jp);
+
+            //Console.Clear();
+            //// ------------------------
+
+            //Random r = new Random();
+            //Bolygo fold = new Bolygo("Föld", "orszagok.txt", r);
+            //Console.WriteLine(fold);
+            //fold.KiirNyelvek("nyelvek.txt");
+        }
+    }
+}
